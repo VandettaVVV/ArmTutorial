fd17447fb38d42236a31768ad11c2ae795285207
Nick Dupre <107683986+LargeFarva144@users.noreply.github.com>
2024-09-10 19:43:57 -0400

    Edited name line somewhere else

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 
-// This code was modified by: *************
+// This code was modified by: Jimmy Neutron
 
 public class Robot extends TimedRobot {
   
